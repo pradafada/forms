84b9aeeb191c9af6bba37bced399339da2f73762
Selah Rice <selah.rice@onedatascan.com>
2024-06-10 16:53:26 -0400

    message

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-form-component',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './form-component.component.html',
   styleUrl: './form-component.component.css'
 })
